3f92a3b0e84f501ade8dec0c7e70d3753ed49fe4
maymikb <123998301+maymikb@users.noreply.github.com>
2024-01-29 18:53:43 -0800

    graphql,express,node,mongodb, homepage navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header/>
     </div>
   )
 }
